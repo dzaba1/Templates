0112bb550d484b8cdf24e723dd8274a0e6ec11b8
Dzaba <blazej.zabialowicz@gmail.com>
2022-05-21 09:59:00 +0200

    App tests

---
+++ VisualStudio/Sources/Dzaba.CmdTemplate/Src/Dzaba.CmdTemplate.Cmd.Tests/AppTests.cs
@@ -0,0 +1,23 @@
+﻿using AutoFixture;
+using Dzaba.CmdTemplate.TestUtils;
+using NUnit.Framework;
+
+namespace Dzaba.CmdTemplate.Cmd.Tests
+{
+    [TestFixture]
+    public class AppTests
+    {
+        private IFixture fixture;
+
+        [SetUp]
+        public void Setup()
+        {
+            fixture = TestFixture.Create();
+        }
+
+        private App CreateSut()
+        {
+            return fixture.Create<App>();
+        }
+    }
+}
