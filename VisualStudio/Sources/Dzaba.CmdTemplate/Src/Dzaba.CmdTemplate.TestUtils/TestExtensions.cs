16701dc3a8ff972814f78297bd8aea31e537f557
Dzaba <blazej.zabialowicz@gmail.com>
2022-05-21 09:43:08 +0200

    Test utils

---
+++ VisualStudio/Sources/Dzaba.CmdTemplate/Src/Dzaba.CmdTemplate.TestUtils/TestExtensions.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+using Dzaba.CmdTemplate.Utils;
+using Moq;
+
+namespace Dzaba.CmdTemplate.TestUtils
+{
+    public static class TestExtensions
+    {
+        public static Mock<T> FreezeMock<T>(this IFixture fixture)
+            where T : class
+        {
+            Require.NotNull(fixture, nameof(fixture));
+
+            return fixture.Freeze<Mock<T>>();
+        }
+    }
+}
---
+++ VisualStudio/Sources/Dzaba.CmdTemplate/Src/Dzaba.CmdTemplate.TestUtils/TestFixture.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+
+namespace Dzaba.CmdTemplate.TestUtils
+{
+    public static class TestFixture
+    {
+        public static IFixture Create()
+        {
+            return new Fixture()
+                .Customize(new AutoMoqCustomization());
+        }
+    }
+}
